fa1d344b4e3f248654a8de2de584e68b6bd94473
ThaDocIzN <rodan72@msn.com>
2019-02-27 04:04:21 -0600

    added repos

---
+++ src/main/java/com/example/zoo/repos/ZooRepository.java
@@ -0,0 +1,18 @@
+package com.example.zoo.repos;
+
+import com.example.zoo.models.Zoos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface ZooRepository extends JpaRepository<Zoos, Long> {
+    public Optional<Zoos> findByZooname(String zooName);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM zoo_animal WHERE zooid = :zooid", nativeQuery = true)
+    void deleteZooFromZooAnimal(long zooid);
+}
