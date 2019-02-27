d9cda0bf70ef1ee841f1f3b63f2129364baa266f
ThaDocIzN <rodan72@msn.com>
2019-02-27 04:37:04 -0600

    added zoo animal class

--- src/main/java/com/example/zoo/repos/ZooAnimalRepository.java
+++ src/main/java/com/example/zoo/repos/ZooAnimalRepository.java
@@ -1,18 +1,14 @@
 package com.example.zoo.repos;
 
-import com.example.zoo.models.Zoos;
+import com.example.zoo.models.ZooAnimal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-public interface ZooRepository extends JpaRepository<Zoos, Long> {
-    public Optional<Zoos> findByZooname(String zooName);
-
+public interface ZooAnimalRepository extends JpaRepository<ZooAnimal, Long> {
     @Transactional
     @Modifying
-    @Query(value = "DELETE FROM zoo_animal WHERE zooid = :zooid", nativeQuery = true)
-    void deleteZoo(long zooid);
+    @Query(value = "DELETE FROM zoo_animal WHERE zooid = :zooid AND animalid = :animalid", nativeQuery = true)
+    void deleteRelationAnimal(long zooid, long animalid);
 }
